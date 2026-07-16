e29b034b8da522739ea3c44de3a46e31c4522a1e
Simalbutt <simalbutt04@gmail.com>
2026-07-16 10:27:21 +0500

    added security through annotations

--- src/main/java/org/example/helloworld/News/ApiSecurityConfiguration.java
+++ src/main/java/org/example/helloworld/News/ApiSecurityConfiguration.java
@@ -3,11 +3,13 @@ package org.example.helloworld.News;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class ApiSecurityConfiguration {
 
     @Bean
@@ -15,10 +17,11 @@ public class ApiSecurityConfiguration {
         http.authorizeHttpRequests(config -> config
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers(HttpMethod.GET,"/api/v1/news","/api/v1/news/**").permitAll()
-                .requestMatchers(HttpMethod.POST,"/api/v1/news","/api/v1/news/**").hasAnyRole("reporter","editor")
-                .requestMatchers(HttpMethod.DELETE,"/api/v1/news/**").hasAnyRole("editor")
-                        .requestMatchers(HttpMethod.PUT,"/api/v1/news/**").hasAnyRole("reporter")
-                        .anyRequest().hasRole("reporter"))
+//                .requestMatchers(HttpMethod.POST,"/api/v1/news","/api/v1/news/**").hasAnyRole("reporter","editor")
+//                .requestMatchers(HttpMethod.DELETE,"/api/v1/news/**").hasAnyRole("editor")
+//                        .requestMatchers(HttpMethod.PUT,"/api/v1/news/**").hasAnyRole("reporter")
+                        .anyRequest().hasRole("REPORTER")
+                )
                 .formLogin(config ->{
                 }).csrf(AbstractHttpConfigurer::disable);
         return http.build();
--- src/main/java/org/example/helloworld/News/Controller.java
+++ src/main/java/org/example/helloworld/News/Controller.java
@@ -1,8 +1,10 @@
 package org.example.helloworld.News;
 
+import jakarta.annotation.security.PermitAll;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.Optional;
@@ -17,6 +19,7 @@ public class Controller {
     }
 
     @GetMapping
+    @PermitAll
     public Map<String,Object> FindAll(
             @RequestParam(required = false, defaultValue = "0" ) int page,
             @RequestParam(required = false, defaultValue = "5") int size)
@@ -27,6 +30,7 @@ public class Controller {
     }
 
     @GetMapping("/{id}")
+    @PermitAll
     public ResponseEntity<News> getNewsById(@PathVariable("id") Long id) {
         Optional<News> value = newsService.findById(id);
 
@@ -36,16 +40,19 @@ public class Controller {
         return ResponseEntity.notFound().build();
     }
 
+    @PreAuthorize("hasAnyRole('REPORTER','EDITOR')")
     @PostMapping
     public ResponseEntity<News> create(@RequestBody News news) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(newsService.create(news));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(newsService.create(news));
     }
-
+    @PreAuthorize("hasRole('REPORTER')")
     @PutMapping("/{id}")
     public ResponseEntity<News> update(@PathVariable("id") long id ,@RequestBody News news) {
         return ResponseEntity.ok(newsService.update(id,news));
     }
 
+    @PreAuthorize("hasRole('EDITOR')")
     @DeleteMapping("/{id}")
     public ResponseEntity<News> delete(@PathVariable long id) {
         newsService.deleteById(id);
